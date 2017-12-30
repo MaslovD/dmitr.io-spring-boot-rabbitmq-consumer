c280597abecc5490ef9022243071c77284e40c93
masdmtr <masdmtr@gmail.com>
2017-12-30 20:37:06 +0300

    initial commit

---
+++ src/main/java/io/dmitr/springbootrabbitmqconsumer/SpringBootRabbitmqConsumerApplication.java
@@ -0,0 +1,12 @@
+package io.dmitr.springbootrabbitmqconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitmqConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRabbitmqConsumerApplication.class, args);
+	}
+}
---
+++ src/test/java/io/dmitr/springbootrabbitmqconsumer/SpringBootRabbitmqConsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package io.dmitr.springbootrabbitmqconsumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootRabbitmqConsumerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
